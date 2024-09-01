7cfeb741218d9b26fe4e1646e690d668b49661ce
Perspective <115461545+Perspective2077@users.noreply.github.com>
2024-09-01 12:58:13 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily : {
+      'bebas' : ["Bebas Neue",'sans-serif'],
+      'nunito' : ["Nunito Sans",'sans-serif'],
+      'nav' : ["Rajdhani",'sans-serif'],
+    }
+  },
+  plugins: [],
+}
